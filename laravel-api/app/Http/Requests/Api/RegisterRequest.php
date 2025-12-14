45ddbb02f70cff8fd05664aaa8acbb4d526ee6ac
teetom3 <thomas.zeryouh@gmail.com>
2025-12-14 15:15:30 +0100

    add form request register and login

--- laravel-api/app/Http/Requests/Api/RegisterRequest.php
+++ laravel-api/app/Http/Requests/Api/RegisterRequest.php
@@ -11,7 +11,7 @@ class RegisterRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            
+            'company_name' => 'required|string',
+            'name'=> 'required|string',
+            'email' => 'required|unique:email,users|email',
+            'password' => 'required|string|min:8|confirmed'
         ];
     }
 }
--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -1,3 +1,19 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+
+
+
+Route::post('/register',[AuthController::class , 'register'] );
+
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:santcum')->group(function(){
+
+    Route::get('/getMe' , [AuthController::class, 'getMe']);
+
+    Route::get('/logout', [AuthController::class, 'logout']);
+
+});
